75ebdf2ec4176d97fb11ff197889e802f4199f6b
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-04-15 18:49:13 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -16,7 +16,7 @@ public class Sound
   public int limitAmplitude(int limit)
   {  
     int count = 0;
-    for(int i = 0; i < smaples.length; i++){
+    for(int i = 0; i < samples.length; i++){
       if (samples[i] > limit){
         samples[i] = limit;
         count = count + 1;
